2d4ac5b1ea77b13bd843fd48f5cafc1de31d9e6a
Dumilde Matos <dumilde.matos@jupiter.co.ao>
2021-03-19 03:38:11 +0100

    Adicionar react app component

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export default function App() {
+  return (
+    <>
+    <h1>App component again </h1>
+    <button>Notify</button>
+    </>
+  )
+}
