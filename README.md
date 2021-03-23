9b549c2c2f477c1dfe2f0399bfde6b90e440e138
Dumilde Matos <dumilde.matos@jupiter.co.ao>
2021-03-23 23:44:17 +0100

    Create Login View

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # simple-doc-app
 ElectronJs documentation manager
+
+
+# Design System
+https://ant.design/docs/react/introduce 
--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import Login from './views/Login/Login'
 export default function App() {
   return (
     <>
-    <h1>App component again </h1>
-    <button>Notify</button>
+    {/* <h1>App component again </h1>
+    <button>Notify</button> */}
+    <Login />
     </>
   )
 }
